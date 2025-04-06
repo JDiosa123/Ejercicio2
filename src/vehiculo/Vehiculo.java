0b0ccd5baed9c0d8610219ede7605116312fae31
JDIOSA123 <diosariosjuan@gmail.com>
2025-04-06 14:01:03 -0500

    primer commit

---
+++ src/vehiculo/Vehiculo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vehiculo;
+
+/**
+ *
+ * @author j3157
+ */
+public class Vehiculo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
